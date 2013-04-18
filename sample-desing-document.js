91a74fd6b7011451ef9c7171b5723347830dccdb
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-04-18 20:58:43 +0300

    A XML sample show.

--- sample-desing-document.js
+++ sample-desing-document.js
@@ -12,6 +12,7 @@
     }
   },
   "shows": {
-    "recipe": "function (doc, req) { var output; output = '<h1>' + doc.title + '</h1>'; output += '<p>' + doc.subtitle + '</p>'; output += '<h2>Ingredients</h2>'; output += '<ul>'; for (var i = 0;  i < doc.ingredients.length;  i++) {output += '<li>' + doc.ingredients[i].measure + ' ' + doc.ingredients[i].ingredient + '</li>'; }output += '</ul>'; output += '<h2>Method</h2>'; output += '<ol>'; for (var i = 0;  i < doc.method.length;  i++) {output += '<li>' + doc.method[i].text + '</li>'; }output += '</ol>'; return output; }"
+    "recipe": "function (doc, req) { var output; output = '<h1>' + doc.title + '</h1>'; output += '<p>' + doc.subtitle + '</p>'; output += '<h2>Ingredients</h2>'; output += '<ul>'; for (var i = 0;  i < doc.ingredients.length;  i++) {output += '<li>' + doc.ingredients[i].measure + ' ' + doc.ingredients[i].ingredient + '</li>'; }output += '</ul>'; output += '<h2>Method</h2>'; output += '<ol>'; for (var i = 0;  i < doc.method.length;  i++) {output += '<li>' + doc.method[i].text + '</li>'; }output += '</ol>'; return output; }",
+    "asxml": "function (doc, req) { return {'body': '<recipe><title>' + doc.title + '</title></recipe>', 'headers': {'Content-type': 'application/json' } }; }"
   }
 }
