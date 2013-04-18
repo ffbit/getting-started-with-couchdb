cee0c0f4d35e4a257875050e6c09d3116747b1b8
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-04-18 20:34:48 +0300

    Non-formated extended show.

--- sample-desing-document.js
+++ sample-desing-document.js
@@ -12,6 +12,26 @@
     }
   },
   "shows": {
-    "recipe": "function(doc, req) { return '<h1>' + doc.title + '</h1>'; }"
+    "recipe": "function(doc, req) { 
+      var output;
+      output = '<h1>' + doc.title + '</h1>';
+      output += '<p>' + doc.subtitle + '</p>';
+
+      output += '<h2>Ingredients</h2>';
+      output += '<ul>';
+      for (var i = 0; i < doc.ingredients.length; i++) {
+        output += '<li>' + doc.ingredients[i].measure + ' ' + doc.ingredients[i].ingredient + '</li>';
+      }
+      output += '</ul>';
+
+      output += '<h2>Method</h2>';
+      output += '<ol>';
+      for (var i = 0; i < doc.method.length; i++) {
+        output += '<li>' + doc.method[i].text + '</li>';
+      }
+      output += '</ol>';
+
+      return output;
+    }"
   }
 }
