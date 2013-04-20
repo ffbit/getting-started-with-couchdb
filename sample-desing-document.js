b814a5c4aa8219538a2c2524194ac71523f104f4
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-04-20 23:14:59 +0300

    Updated the by_tytle view.

--- sample-desing-document.js
+++ sample-desing-document.js
@@ -5,7 +5,7 @@
       "map": "function(doc) { emit(doc.title, doc);  }"
     },
     "by_title": {
-      "map": "function(doc) { if (doc.title) { emit(doc.title, doc); } }"
+      "map": "function(doc) { if (doc.title) { emit(doc.title, null); } }"
     },
     "by_keyword": {
       "map": "function(doc) { for (var i = 0; i < doc.keywords.length; i++) { emit(doc.keywords[i], doc); } }"
