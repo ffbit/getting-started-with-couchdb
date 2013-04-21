b3531bf27f27419e56e05630b6c35aade32811ee
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-04-21 20:12:42 +0300

    Playing around the group_level parameter for the _stats reduce function.

--- cities-desing-document.js
+++ cities-desing-document.js
@@ -4,6 +4,10 @@
     "bynamecity": {
       "map": "function(doc) { if (doc.name && doc.city) { emit([doc.name, doc.city], doc.sales); } }",
       "reduce": "_sum"
+    },
+    "bynamecity_stats": {
+      "map": "function(doc) { if (doc.name && doc.city) { emit([doc.name, doc.city], doc.sales); } }",
+      "reduce": "_stats"
     }
   }
 }
---
+++ view-bynamecity-stats.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source config.sh
+
+URL=http://$HOST:$PORT/$DATABASE/_design/cities/_view/bynamecity_stats
+
+echo "Playing around the group_level parameter"
+
+for i in {0..2}
+do
+  echo "group_level=$i"
+  read -p "Press enter to continue..."
+  curl -X GET $URL?group_level=$i | jshon -S
+done
