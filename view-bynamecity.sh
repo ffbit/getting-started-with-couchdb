91424714b53c89ac696b376733f581f33146745e
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-04-21 19:54:03 +0300

    Playing around the group_level parameter for the _sum reduce function.

--- view-bynamecity.sh
+++ view-bynamecity.sh
@@ -2,4 +2,13 @@
 
 source config.sh
 
-curl -X GET http://$HOST:$PORT/$DATABASE/_design/cities/_view/bynamecity | jshon -S
+URL=http://$HOST:$PORT/$DATABASE/_design/cities/_view/bynamecity
+
+echo "Playing around the group_level parameter"
+
+for i in {0..2}
+do
+  echo "group_level=$i"
+  read -p "Press enter to continue..."
+  curl -X GET $URL?group_level=$i | jshon -S
+done
